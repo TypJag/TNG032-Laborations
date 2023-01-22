8dec0e8a55e15a4d02b46ff5afd1306534da5960
TypJag <Axel.andersson42@gmail.com>
2023-01-22 17:03:58 +0100

    Completed Lab1
    Completed lab1

--- Lab1/Lab1.m
+++ Lab1/Lab1.m
@@ -1,11 +1,15 @@
 
-%distance_map(16,16);
+distance_map = zeros(16,16);
 
-%for n = 1:1:16
-%    for m = 1:1:16
-%        distance_map(n,m) = manhat(n,m,9,9);
-%    end
-%end
+for n = 1:1:16
+    for m = 1:1:16
+        distance_map(n,m) = manhat(n,m,9,9);
+    end
+end
 
-distance_map(16,16) = manhat(n,m,9,9);
-figure; imagesc(distance_map);
+
+
+%distance_map(16,16) = manhat(n,m,9,9);
+
+distance_map
+%figure; imagesc(distance_map);
